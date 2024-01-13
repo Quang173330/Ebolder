94f3f53a4bee946ceb165bbf245ccb98a48d4058
Quang173330 <Quang.lxun@gmail.com>
2024-01-14 02:13:42 +0700

    change open link

--- resources/js/components/frontend/learn/LearnPage.vue
+++ resources/js/components/frontend/learn/LearnPage.vue
@@ -103,7 +103,7 @@
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M5.25 12C5.25 11.5858 5.58579 11.25 6 11.25L18 11.25C18.4142 11.25 18.75 11.5858 18.75 12C18.75 12.4142 18.4142 12.75 18 12.75L6 12.75C5.58579 12.75 5.25 12.4142 5.25 12Z" fill="#141416"/>
               </svg>
               <span class="ml-2">
-                Quay lại
+                Back
               </span>
             </a>
             <a  href="/history" :class="[`flex bg-${lessonType} py-2 rounded cursor-pointer justify-center` ]" style="width: 50%">
